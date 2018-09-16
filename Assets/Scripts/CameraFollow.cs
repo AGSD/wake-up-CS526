2dfd39f869bf4d05b76e9c0cdd4e05cee1724543
Aditya Gaur <adityaga@usc.edu>
2018-09-15 19:39:32 -0700

    Adding back normal files

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Vector3 offset;
+    public GameObject player;
+	public float lerpRate;
+
+	// Use this for initialization
+	void Start () {
+		offset = player.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		Vector3 target = player.transform.position - offset;
+		pos = Vector3.Lerp (pos, target, lerpRate * Time.deltaTime);
+		transform.position = pos;
+	}
+}
