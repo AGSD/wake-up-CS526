af290fba3eafd255b474049e993cc090aee5ba31
Raghav-Prakash <raghrav2995@gmail.com>
2018-09-18 00:34:46 -0700

    Add collision-animations, prefab-tags and rearrange floors-prefab in hierarchy

---
+++ Assets/Boss/Player_bossLevel_movements.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_bossLevel_movements : MonoBehaviour {
+    public float speed = 10.0F;
+    public float rotationSpeed = 500.0F;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -7,6 +7,14 @@ public class Boundary {
     public float xMin, xMax;
 }
 
+// collision properties
+[System.Serializable]
+public class Collisions {
+    public bool didCollide = false;
+    public bool didFallFlat = false;
+    public bool didCollideSideways = false;
+}
+
 public class PlayerMotor : MonoBehaviour {
 
     private Animator animator;
@@ -22,6 +30,9 @@ public class PlayerMotor : MonoBehaviour {
 
     public float gravity;
 
+    public Collisions collisions;
+
+
     // Use this for initialization
     void Start () {
         controller = GetComponent<CharacterController>();
@@ -33,23 +44,37 @@ public class PlayerMotor : MonoBehaviour {
 
         if(controller.isGrounded) {
 
-            moveDirection = new Vector3(
-                Input.GetAxis("Horizontal") * speedXAxis, 
-                0, 
-                speed
-            );
+            if(collisions.didCollide) {
+                moveDirection = new Vector3(
+                    Input.GetAxis("Horizontal") * speedXAxis,
+                    0,
+                    0
+                );
+            }
+            if (collisions.didFallFlat || collisions.didCollideSideways) {
+                moveDirection = Vector3.zero;
+            }
+
+            if(!collisions.didCollide && !collisions.didFallFlat && !collisions.didCollideSideways) {
+                moveDirection = new Vector3(
+                    Input.GetAxis("Horizontal") * speedXAxis,
+                    0,
+                    speed
+                );
+            }
 
             moveDirection = transform.TransformDirection(moveDirection);
 
             if (Input.GetKey(KeyCode.UpArrow)) {
                 animator.SetTrigger("Jump");
                 moveDirection.y = jumpSpeed;
+                moveDirection.z += speed * 15;
             }
             if (Input.GetKey(KeyCode.DownArrow)) {
                 animator.SetTrigger("Slide");
                 moveDirection.z += speed;
             }
-         }
+        }
         else {
             moveDirection.x = Input.GetAxis("Horizontal") * speed;
             moveDirection.z = speed;
@@ -68,9 +93,16 @@ public class PlayerMotor : MonoBehaviour {
     }
 
     private void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("FlameRow") || other.CompareTag("FireBall") || other.CompareTag("LargeFlames")) {
-            animator.SetTrigger("Die");
-            Debug.Log("Collison with " + other.tag);
+        if(other.CompareTag("TableStumble")) {
+            animator.SetTrigger("Stumble");
+        }
+
+        if (other.CompareTag("TripOver")) {
+            animator.SetTrigger("Fall Flat");
+        }
+
+        if (other.CompareTag("TripSideways")) {
+            animator.SetTrigger("Stumble Sideways");
         }
     }
 }
