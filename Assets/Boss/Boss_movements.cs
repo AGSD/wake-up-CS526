d524475d5db9475420635890d9d220f73b45ba79
akshaybhatia95 <akshaybhatia95@gmail.com>
2018-10-14 18:09:10 -0700

    added level 2 boss

--- Assets/Boss/Boss_movements.cs
+++ Assets/Boss/Boss_movements.cs
@@ -1,26 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Boss_movements : MonoBehaviour
 {
-    public Transform goal;
+    public GameObject goal;
     public float accuracy;
     public float speed;
+    public Slider HealthBar;
+    Animator anim;
+    private bool bossMoveFlag = true;
+    public void setMoveBool(bool val) {
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isAttacking1", false);
+        anim.SetBool("isAttacking2", false);
+        bossMoveFlag = false;
+    }
     // Use this for initialization
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        this.transform.LookAt(goal.position);
-        Vector3 direction = goal.position - this.transform.position;
-        if (direction.magnitude > accuracy)
+    {
+        if (bossMoveFlag)
         {
-            this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+            this.transform.LookAt(goal.transform.position);
+            Vector3 direction = goal.transform.position - this.transform.position;
+            if (direction.magnitude > accuracy)
+            {
+                this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+                anim.SetBool("isRunning", true);
+                anim.SetBool("isAttacking1", false);
+                anim.SetBool("isAttacking2", false);
+            }
+            else
+            {
+                anim.SetBool("isRunning", false);
+                int number=Random.Range(1,3);
+                anim.SetBool("isAttacking"+number, true);
+
+            }
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            HealthBar.value -= 1;
+            Debug.Log("Bullet Hit");
+            Destroy(other.gameObject, 0f);
+        }
+
+        if (HealthBar.value <= 0)
+        {
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isAttacking1", false);
+            anim.SetBool("isAttacking2", false);
+            anim.SetBool("isDead", true);
+            StartCoroutine(killPlayer(3));
+        }
+
+    }
+    IEnumerator killPlayer(float seconds)
+    {
+        float currentTime = 0;
+        bossMoveFlag = false;
+        goal.GetComponent<Player_bossLevel_movements>().setMoveBool(false);
+        
+        while (currentTime < seconds)
+        {
+            Debug.Log("CO ROUTINE LOOP");
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+      //  SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 }
--- Assets/Boss/Player_bossLevel_movements.cs
+++ Assets/Boss/Player_bossLevel_movements.cs
@@ -1,22 +1,93 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class Player_bossLevel_movements : MonoBehaviour {
+    public GameObject boss;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    public Slider HealthBar;
     public float speed = 10.0F;
     public float rotationSpeed = 500.0F;
+    private Animator anim;
+    private bool playerMoveFlag = true;
+
+    public void setMoveBool(bool val) {
+        playerMoveFlag = val;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            HealthBar.value -= 5;
+            Debug.Log("HitSmall");
+        }
+        if (other.gameObject.tag == "Enemy2")
+        {
+            HealthBar.value -= 10;
+            Debug.Log("HitBIG");
+        }
+
+
+        if (HealthBar.value <= 0) {
+            anim.SetBool("isDead", true);
+            StartCoroutine(killPlayer(3));
+        }
+       
+    }
     // Use this for initialization
     void Start () {
-		
+        anim = GetComponent<Animator>();
 	}
 
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
+        if (playerMoveFlag)
+        {
+            float translation = Input.GetAxis("Vertical") * speed;
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
+            transform.Translate(0, 0, translation);
+            transform.Rotate(0, rotation, 0);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Fire();
+            }
+        }
+
+    }
+    IEnumerator killPlayer(float seconds) {
+        float currentTime = 0;
+        playerMoveFlag = false;
+        boss.GetComponent<Boss_movements>().setMoveBool(false);
+       
+        while (currentTime < seconds)
+        {
+            Debug.Log("CO ROUTINE LOOP");
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.forward * 50;
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 5f);        
     }
 }
